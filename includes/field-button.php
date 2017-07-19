3b7646baba5930bbed9c0d03b917f8f2abb1f4f0
Achal Jain <achal@ideaboxcreations.com>
2017-07-19 11:56:00 +0530

    Added button field

---
+++ includes/field-button.php
@@ -0,0 +1,2 @@
+<span class="button mbt-field-button<?php if ( isset( $field['class'] ) ) echo ' '. $field['class']; ?>" href="javascript:void(0);" onclick="return false;"><?php echo $field['label']; ?></span>
+<span class="mbt-loader"><img src="<?php echo admin_url('images/spinner.gif'); ?>" /></span>
